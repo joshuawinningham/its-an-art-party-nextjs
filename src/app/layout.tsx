b38a0a2ee683d4c2b2b8b672508a038cd18c9510
joshuawinningham <joshua.winningham@gmail.com>
2026-03-03 21:55:14 -0700

    Add canonical URLs and robots meta tag to all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,20 @@ export const metadata: Metadata = {
   icons: {
     icon: "/images/favicon.png",
   },
+  alternates: {
+    canonical: "./",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
   openGraph: {
     type: "website",
     siteName: "It's an Art Party",
